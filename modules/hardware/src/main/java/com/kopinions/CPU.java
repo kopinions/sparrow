5ccf0ae8be74a328c4b784d08292caa3851e2df0
sjkyspa <sjkyspa@gmail.com>
2020-01-24 18:17:31 +0800

    feat: test support with junit5

---
+++ modules/hardware/src/main/java/com/kopinions/CPU.java
@@ -0,0 +1,5 @@
+package com.kopinions;
+
+public class CPU {
+
+}
---
+++ modules/hardware/src/test/java/com/kopinions/CPUTest.java
@@ -0,0 +1,14 @@
+package com.kopinions;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+import org.junit.jupiter.api.Test;
+
+class CPUTest {
+
+  @Test
+  public void should_() {
+    assertTrue(true);
+  }
+}
